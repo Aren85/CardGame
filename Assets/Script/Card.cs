c5c0f70f04bfa472b619fc4d560846d05ddc0492
Aren85 <a00123561574@gmail.com>
2023-10-05 22:23:58 +0800

    創建放置卡牌區域

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Custom/Card")]
+public class Card : ScriptableObject
+{
+    public string cardName;     // 卡牌的名稱
+    public string description;  // 卡牌的描述
+    public Sprite cardImage;    // 卡牌的圖像
+    public CardSuit suit;       // 使用CardSuite枚举来花色
+    public CardValue cardValue; // 使用CardValue枚举来存储点数
+
+    public enum CardSuit
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+
+    public enum CardValue
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+
+}
